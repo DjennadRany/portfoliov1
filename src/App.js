804d63650ed3ebbbe741f5e138573bacddf1a66b
DjennadRany <djennad.ar@gmail.com>
2023-04-11 16:25:45 +0200

    version7

--- src/App.js
+++ src/App.js
@@ -14,18 +14,18 @@ function App() {
   return (
     <><body>
       <div className="container">
-
+      <p>Chef de projet digital - webmaster - développeur front-end : Angular, react, vus.js, aem<br></br>
+          Directement par mail à l'adresse : <a href="mailto:djennad.ar@gmail.com.">Djennad.ar@gmail.com. </a></p>
     
         <Nav activeKey="/" onSelect={(selectedKey) => `selected ${selectedKey}`}>
-          <Nav.Item><Nav.Link href="/">Home</Nav.Link></Nav.Item>
-         
+
 
         </Nav>
         <img class="log" src={logo} alt="logo"/>
 
         <BrowserRouter>
           <Routes>
-            <Route path="/" element={<Home />} />home
+            <Route path="portfoliov1/" element={<Home />} />home
             <Route path="/About" element={<About />} />
             <Route path="/Colla" element={<Colla />} />
             <Route path="/Contact" element={<Contact />} />
@@ -33,8 +33,7 @@ function App() {
         </BrowserRouter>
       </div>
     </body><footer>
-        <p>Chef de projet digital - webmaster - développeur front-end : Angular, react, vus.js, aem<br></br>
-          Directement par mail à l'adresse : <a href="mailto:djennad.ar@gmail.com.">Djennad.ar@gmail.com. </a></p>
+        
       </footer></>
 
     
